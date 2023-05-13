9289a50ec93843295a1be29ad6e8a40577968e53
thinkvantagedu <duxiaohan2006@gmail.com>
2023-05-13 23:34:22 +0100

    Add top bar

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,23 @@
+import styles from '../page.module.scss'
+export const Navbar = () => {
+  return (
+    <nav className="bg-white-800 w-full">
+      <div className="flex flex-col justify-between items-center p-6">
+        <div className={styles.description}>
+          <p>
+            COMING SOON...
+          </p>
+          <div>
+            <a
+              href="https://sanctumwellbeing.co.uk/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              LOGO
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav >
+  )
+}
