f0fb5c1a1e5f943dfb5ea4ca66ef2a0b6b448e1a
thinkvantagedu <duxiaohan2006@gmail.com>
2023-05-13 15:08:28 +0100

    Coming Soon page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,86 +1,84 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.scss'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          COMING SOON...
         </p>
         <div>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://sanctumwellbeing.co.uk/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            LOGO
           </a>
         </div>
       </div>
-
       <div className={styles.center}>
         SANCTUM WELLBEING
       </div>
 
       <div className={styles.grid}>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.aor.org.uk/what-is-reflexology/"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Hello <span>-&gt;</span>
+            Reflexology <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>Find More</p>
         </a>
 
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.onsitemassageco.com/blog/reflexology-can-improve-physical-mental-wellbeing/"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Learn <span>-&gt;</span>
+            Wellbeing <span>-&gt;</span>
           </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          <p>Find More</p>
         </a>
 
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.aor.org.uk/custom/far-search/map.php?lat=51.483707&lng=-3.1680962&country=GB&location=Cardiff%2C+UK&name=Louise+"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Templates <span>-&gt;</span>
+            Assiciation of Reflexologists Verified <span>-&gt;</span>
           </h2>
-          <p>Explore the Next.js 13 playground.</p>
+          <p>Find More</p>
         </a>
 
         <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://search.cnhcregister.org.uk/search?name=+louise+davies&therapy=14&radius=&city=cardiff&postcode=&g-recaptcha-response=03AL8dmw8RSKXDxpdAFH6QSK3lrPckmB9OWoMof2tqsZBnRY33vmKC5WHAW92uAGw8dNGRuTmRbUIlNgVfhPUACwTzFQhdCyie9W4i4BuuBhtYyzHiZkWOznbCHIqiVuSTJRsIobR-EsR5ys6AP_WAVXoPV9WDpQoLKtgqzno8mnmtF0e50ukxGufYztLW6hfN4j6S8tHM3_8HuiPDzMprswvp3TFFm4LsYiKCju_8fym252wejFaRFee7mvkzxlpZWcJh7vygVM7IsMTwkwSrdDUj1mmt_5Kk6TCEflAHM6upySD1NDyPXLr2SioZfsKukMlOEjaKHL2hdliUXnowOPac1vlZR8U95FKrQsHtOEKWSrX6KgsqeqPnjZmZEFWut3hylTCyZjHZINIFu-SF1X-y32nX911WJb6GEyzrz6wnINgjGxFulhmRM7yQDuPQUmqM_02Jf73rh6lHW77ldpEaN0TVs6i2qTJaNq7Oxf6x0DSjKOdMlHhHbuT3pznBXdeIOgHotGqj_t4yAvqjKhUEIGC-PC-IeZzw6Rj-f541wwZoooSPSJUXRdajwOtpi-WkeCWfUli7sJDZrTQ-1cMQ8mXE1ABn1YxnIRVxQ1aD_dzNFoyeo1efcR1wZG-XCx-R6pp-1edQ"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Deploy <span>-&gt;</span>
+            CNHC Verified <span>-&gt;</span>
           </h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Find More
           </p>
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://xiaohan-du.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Website built by Xiaohan Du
         </a>
       </div>
     </main>
