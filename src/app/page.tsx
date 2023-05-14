07f3173c9361c40b97b348484426e653d8cac32c
thinkvantagedu <duxiaohan2006@gmail.com>
2023-05-14 23:00:52 +0100

    Change font, other minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,15 @@ import { Banner } from './components/banner';
 export default function Home() {
   return (
     <>
-      <main className={`${styles.main} max-w-screen-xl`}>
+      <main className={`${styles.main} max-w-screen-xl font-serif font-thin`}>
         <Navbar />
 
-        <div className={styles.center}>
+        <div className={`${styles.center}`}>
           SANCTUM WELLBEING
         </div>
 
         <Banner
-          title="Introduction"
+          title=""
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
         />
 
