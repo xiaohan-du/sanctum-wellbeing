6694eb65aedb80771984577fb2982d17fc494a37
thinkvantagedu <duxiaohan2006@gmail.com>
2023-05-17 21:07:19 +0100

    Add logo, change bg color

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,8 +1,11 @@
-import styles from '../page.module.scss'
+import styles from './navbar.module.scss';
+import logo from '../static/images/logo.jpeg';
+import Image from 'next/image';
+
 export const Navbar = () => {
   return (
-    <nav className="bg-white-800 w-full">
-      <div className="flex flex-col justify-between items-center">
+    <nav className={`bg-white-800 w-screen ${styles.navbar}`}>
+      <div className="flex flex-col justify-between items-center max-w-screen-xl m-auto">
         <div className={styles.description}>
           <p>
             COMING SOON...
@@ -13,7 +16,7 @@ export const Navbar = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              LOGO
+              <Image src={logo} alt="Logo" className='h-32 w-auto'/>
             </a>
           </div>
         </div>
