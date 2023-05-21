0e7b101bcfd95c617832a75c0a92c1eeeed35589
thinkvantagedu <duxiaohan2006@gmail.com>
2023-05-21 10:58:19 +0100

    Change general layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import styles from './page.module.scss';
-import { Navbar } from './components/navbar';
+import { MainBanner } from './components/mainBanner';
 import { Banner } from './components/banner';
 import { IgGrid } from './components/igGrid';
 import { igUrls } from './static/data/igUrls';
@@ -13,11 +13,7 @@ export default function Home() {
   return (
     <>
       <main className={`${styles.main} max-w-screen-xl font-serif font-thin tracking-normal`}>
-        <Navbar />
-
-        <div className={`${styles.center} tracking-widest`}>
-          SANCTUM WELLBEING
-        </div>
+        <MainBanner />
         <div className='mb-16'>
           <Banner
             title="The Business"
@@ -49,17 +45,6 @@ export default function Home() {
           <CardRow cardRowData={workplaceData} />
         </div>
 
-        <div className='mb-16'>
-          <Banner
-            title="Link / stories to Health Shield/ Deloitte / Transport for Wales / Willows High School"
-            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-          />
-        </div>
-        <div className='-mt-28 mb-16 z-10'>
-          <CardRow cardRowData={coorporateData} />
-        </div>
-
-        <IgGrid title={'See what my customer says...'} urls={igUrls} />
         <div className='w-full flex justify-center items-end h-12'>
           <a
             href="https://xiaohan-du.com"
