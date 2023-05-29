378777e00e205916a1c881b4798582e8cc471747
thinkvantagedu <duxiaohan2006@gmail.com>
2023-05-29 23:21:23 +0100

    General layout 5

--- src/app/components/cardRow.tsx
+++ src/app/components/cardRow.tsx
@@ -47,7 +47,7 @@ export const CardRow = ({ cardRowData }: ICardRow) => {
           >
             {
               title ? (
-                <h2 className='text-xl lg:text-base md:text-sm'>
+                <h2 className='text-xl lg:text-base'>
                   {title}
                 </h2>
               ) : null
@@ -55,7 +55,7 @@ export const CardRow = ({ cardRowData }: ICardRow) => {
             <p className='font-normal lg:text-sm md:text-xs'>{content}</p>
             {
               imgUrl ? (
-                <Image src={imgUrl} alt="Logo" className='h-28 xl:h-24 lg:h-16 md:h-16 md:w-16 w-auto' />
+                <Image src={imgUrl} alt="Logo" className='h-28 xl:h-24 lg:h-16 md:h-20 md:w-20 w-auto' />
               ) : null
             }
           </a>
