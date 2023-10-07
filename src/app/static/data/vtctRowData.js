8f45c91262eec2db619565e659b20d6f22cdc16e
thinkvantagedu <duxiaohan2006@gmail.com>
2023-10-07 23:47:00 +0100

    Test removing image

--- src/app/static/data/vtctRowData.js
+++ src/app/static/data/vtctRowData.js
@@ -1,9 +1,8 @@
-import vtct from '../images/vtct.jpeg';
 import styles from '../../components/imageRow.module.scss';
 
 export const vtctRowData = [
   {
     classes: `${styles.container} h-24 lg-h-10 md-h-6 sm-h-10 w-auto p-2`,
-    imgUrl: vtct
+    imgUrl: ''
   }
 ];
