ed48f0ac6c0a54ad3c42c5ef0e6f3f2a592cfa3c
thinkvantagedu <duxiaohan2006@gmail.com>
2023-10-07 23:51:01 +0100

    Re-add PNG image

--- src/app/static/data/vtctRowData.js
+++ src/app/static/data/vtctRowData.js
@@ -1,8 +1,9 @@
+import vtct from '../images/vtct.png';
 import styles from '../../components/imageRow.module.scss';
 
 export const vtctRowData = [
   {
     classes: `${styles.container} h-24 lg-h-10 md-h-6 sm-h-10 w-auto p-2`,
-    imgUrl: ''
+    imgUrl: vtct
   }
 ];
