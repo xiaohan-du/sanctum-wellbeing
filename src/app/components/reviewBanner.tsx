e53fbb06010e00558b57f3d461d2241baf850a25
thinkvantagedu <duxiaohan2006@gmail.com>
2024-03-19 20:46:59 +0000

    Adjust booking btn

--- src/app/components/reviewBanner.tsx
+++ src/app/components/reviewBanner.tsx
@@ -27,7 +27,7 @@ export const ReviewBanner = ({ reviewData }: IReviewBanner) => {
       </div>
       <div className='flex flex-row justify-center w-full mt-16'>
         <a href="https://www.google.com/maps/place/Sanctum+Wellbeing/@51.4922097,-3.205645,17z/data=!4m8!3m7!1s0x486e1bd061c1aea3:0x37d7f4e7ec5b7a50!8m2!3d51.4922097!4d-3.2030647!9m1!1b1!16s%2Fg%2F11szbq1_1v?authuser=0" target="_blank" rel="noopener noreferrer">
-          <button className={`${btnStyles.readMore} font-sans font-medium text-lg md:text-base sm:text-sm`}>
+          <button className={`${btnStyles.basic} font-sans font-medium text-lg md:text-base sm:text-sm`}>
             More Reviews
           </button>
         </a>
--- src/app/static/data/adData.js
+++ src/app/static/data/adData.js
@@ -1,9 +1,5 @@
-import fresha from '../images/Fresha_logo.svg';
-
 export const adData =
 {
-  title: "WE'RE NOW ON",
-  imgUrl: fresha,
   content: "Book an appointment",
   url: 'https://www.fresha.com/a/sanctum-wellbeing-cardiff-uk-88-fields-park-road-z5cvax7v'
 };
