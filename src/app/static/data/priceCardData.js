e099a72adbcfca7d38349c942a158349f30c5e27
thinkvantagedu <duxiaohan2006@gmail.com>
2025-06-23 09:32:17 +0100

    230625: revert price data for image issue

--- src/app/static/data/priceCardData.js
+++ src/app/static/data/priceCardData.js
@@ -5,40 +5,35 @@ import backHands from '../images/backHands.png';
 import ladyCream from '../images/ladyCream.png';
 import whiteSheet from '../images/whiteSheet.png';
 import hotStone from '../images/hotStone.png';
-import sportsMassage from '../images/sportsMassage.jpeg';
 
 export const priceCardData = [
   {
     title: 'Reflexology feet/hands or mix',
-    time1: '30 mins',
-    price1: '£40',
-    time2: '60 mins',
-    price2: '£55',
+    time1: '60 mins',
+    price1: '£50',
     imgUrl: handFeet,
     description: ["A super effective treatment for stress reduction, promoting deep relaxation and wellbeing, also great for aiding sleep. It is based on the theory that different reflex points on the feet, lower leg, hands, reflect different body systems and organs. Pressure is applied to reflex points on the feet to help the body relax and return to a state of balance and calmness."]
   },
   {
     title: 'Thai Foot Massage',
-    time1: '30 mins',
-    price1: '£40',
-    time2: '60 mins',
-    price2: '£55',
+    time1: '60 mins',
+    price1: '£50',
     imgUrl: foot,
     description: ["A mix of reflexology, acupressure, and massage of the feet and lower legs, the primary purpose is to open the Thai Sen Energy Channels and help the flow of energy through the body. It relieves not only the discomfort coming from muscular pain, but also treats energetic imbalance circulating through our bodies, helping the correct functioning of body and mind. Invigorating and relaxing."]
   },
   {
     title: 'Indian Head Massage',
     time1: '60 mins',
-    price1: '£55',
+    price1: '£50',
     imgUrl: asian,
     description: ["This incredibly relaxing reatment includes a full back, neck, shoulder massage then onto the head and scalp using a range of IHM techniques, focusing on massaging marma acupressure points along the face, head, scalp, neck. Leaving you in a Zen like state."]
   },
   {
     title: "‘The Ultimate’",
     time1: '90 mins',
-    price1: '£80',
+    price1: '£75',
     time2: '2 hours',
-    price2: '£110*',
+    price2: '£100*',
     imgUrl: whiteSheet,
     description: ["A super stress busting treatment consisting of a full Indian Head Massage, back, neck, shoulders massage, arms, hands, face, head, finishing with feet Reflexology.",
       "* 2 hour bespoke package includes legs, and focuses on wherever you need it most on the day. Ultimate relaxation. Complimentary face mask if required."]
@@ -46,49 +41,38 @@ export const priceCardData = [
   {
     title: 'Bliss Aromatherapy Facial Massage',
     time1: '60 mins',
-    price1: '£65',
+    price1: '£60',
     time2: '90 mins',
-    price2: '£90*',
+    price2: '£85*',
     time3: '2 hours',
-    price3: '£125*',
+    price3: '£110*',
     imgUrl: ladyCream,
-    description: ["‘Not just a facial’. A luxurious facial with massage with relaxation at the heart of this treatment. Indian head massage techniques, with head, face, neck, chest, arms, hand massage complete this blissful facial leaving your skin cleansed, hydrated, and refreshed.", 
-    "* 90 minutes includes full back, neck and shoulder massage.", 
-    "* 2 hours includes Feet Reflexology"]
+    description: ["‘Not just a facial’. A luxurious facial with massage with relaxation at the heart of this treatment. Indian head massage techniques, with head, face, neck, chest, arms, hand massage complete this blissful facial leaving your skin cleansed, hydrated, and refreshed.",
+      "* 90 minutes includes full back, neck and shoulder massage.",
+      "* 2 hours includes Feet Reflexology"]
   },
   {
     title: 'Full body Massage',
+    time1: '60 mins',
+    price1: '£60',
+    time2: '90 mins',
+    price2: '£85',
+    time3: '2 hours',
+    price3: '£110*',
+    imgUrl: backHands,
+    description: ["Ease away your aching muscles and slip into a blissful state of relaxation with this full body massage, tailored to your individual needs on the day. From gentle, medium to firm pressure catered uniquely for you.",
+      "* 2 hours includes Feet Reflexology"]
+  },
+  {
+    title: 'Sanctum Hot Stones Massage',
     time1: '60 mins',
     price1: '£70',
     time2: '90 mins',
     price2: '£100',
     time3: '2 hours',
-    price3: '£125*',
-    imgUrl: backHands,
-    description: ["Ease away your aching muscles and slip into a blissful state of relaxation with this full body massage, tailored to your individual needs on the day. From gentle, medium to firm pressure catered uniquely for you.", 
-    "* 2 hours includes Feet Reflexology"]
-  },
-  {
-    title: 'Sanctum Hot Stones Massage',
-    time1: '60 mins',
-    price1: '£75',
-    time2: '90 mins',
-    price2: '£110',
-    time3: '2 hours',
-    price3: '£130*',
+    price3: '£135*',
     imgUrl: hotStone,
-    description: ["Eradicate stress, tension, anxiety, achy muscles with Sanctums Hot Stone Therapy designed to induce serenity and tranquility throughout the body and mind. This full body massage with hot stone therapy helps ease away tension both in your spirit and body, helping you realign inside and out, leaving you in a most peaceful state.", 
-    "* 2 hours includes feet Reflexology."]
-  },
-  {
-    title: 'Sanctum Sports Massage',
-    time1: '30 mins',
-    price1: '£40',
-    time2: '60 mins',
-    price2: '£70',
-    imgUrl: sportsMassage,
-    description: ["Designed to help you reach peak performance, reduce the risk of injury, and accelerate recovery. We offer personalised pre and post event massage, and maintenance massage. Tailored to meet the unique needs of athletes and active individuals, our expert techniques target specific muscle groups to improve flexibility, increase range of motion, and help alleviate pain. ", 
-      "Immerse yourself in the soothing atmosphere of Sanctum Wellbeing, where our serene environment quiets the body and mind while you experience the transformative benefits of Sports Massage Therapy and Relaxation at the same time. I am committed to creating an unparalleled experience that not only promotes physical recovery but also nurtures mental well-being, stress reduction and relaxation."
-    ]
+    description: ["Eradicate stress, tension, anxiety, achy muscles with Sanctums Hot Stone Therapy designed to induce serenity and tranquility throughout the body and mind. This full body massage with hot stone therapy helps ease away tension both in your spirit and body, helping you realign inside and out, leaving you in a most peaceful state.",
+      "* 2 hours includes feet Reflexology."]
   }
 ];
