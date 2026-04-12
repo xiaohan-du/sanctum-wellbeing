22267c7aa45d3bb2018cb66440a826589b1c2087
thinkvantagedu <duxiaohan2006@gmail.com>
2026-04-12 23:27:38 +0100

    Adjust price and opening hrs

--- src/app/static/data/voucherRowData.js
+++ src/app/static/data/voucherRowData.js
@@ -1,17 +1,17 @@
 export const voucherRowData = [
   {
-    title: 'Reflexology',
+    title: 'Reflexology & Thai foot massage',
     time: '60 mins',
-    price: '£45'
+    price: '£60',
   },
   {
     title: 'Indian Head Massage',
-    time: '45 mins',
-    price: '£40'
+    time: '60 mins',
+    price: '£60',
   },
   {
-    title: "‘The Ultimate’",
+    title: '‘The Ultimate’',
     time: '90 mins',
-    price: '£75'
-  }
+    price: '£90',
+  },
 ];
