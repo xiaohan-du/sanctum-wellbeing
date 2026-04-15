8fb56f9201b392a71c91171d80a9181575cb2dda
thinkvantagedu <duxiaohan2006@gmail.com>
2026-04-15 23:37:02 +0100

    Add gift voucher page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,19 +8,18 @@ import { ReviewBanner } from './components/reviewBanner';
 import { reviewData } from './static/data/reviewData';
 import { mediaRowData } from './static/data/mediaRowData';
 import { ContactRow } from './components/contactRow';
-import massage from './static/images/massage.png';
 import lady from './static/images/lady.png';
 import { Footer } from './components/footer';
 import { OpeningHours } from './components/openingHours';
 import { Ad } from './components/ad';
 import { adData } from './static/data/adData';
-import { giftVoucherData } from './static/data/giftVoucherData';
 import { PriceCardBox } from './components/priceCardBox';
 import { priceCardData } from './static/data/priceCardData'
 import { AboutPromoBanner } from './components/aboutPromoBanner';
 import { LymphaticPromoBanner } from './components/lymphaticPromoBanner';
 import { ReflexologyPromoBanner } from './components/reflexologyPromoBanner';
 import { CorporatePromoBanner } from './components/corporatePromoBanner';
+import { GiftVoucherPromoBanner } from './components/giftVoucherPromoBanner';
 import { BackToTop } from './components/backToTop';
 
 export default function Home() {
@@ -61,22 +60,7 @@ export default function Home() {
           <PriceCardBox priceCardData={priceCardData}/>
         </div>
 
-        <div className='mb-16'>
-          <Banner
-            title="Gift Vouchers"
-            bannerHeight='h-auto'
-            negativeTitleMargin='-mt-4 xl:-mt-8'
-            imgUrl={massage}
-            content={[
-              'Looking for a thoughtful gift that truly makes a difference?',
-              'Sanctum Wellbeing gift vouchers are perfect for anyone who could benefit from time to relax, unwind, and feel their best.',
-              'Available for all treatments, including massage, reflexology, and lymphatic drainage, vouchers can be tailored to suit your needs.',
-              'Whether it’s for a birthday, special occasion, or simply to show someone you care, a treatment experience is a gift they’ll genuinely appreciate.',
-              'Purchase a gift voucher today or get in touch for more information.',
-            ]}
-          />
-          <Ad adData={giftVoucherData}/>
-        </div>
+        <GiftVoucherPromoBanner />
 
         <div className='mb-16'>
           <Banner
